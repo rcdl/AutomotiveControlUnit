a11e3e53d4b35d32871a5766deb63fcffff626c4
Rodrigo César <rcdl@cin.ufpe.br>
2018-11-10 16:05:39 -0300

    Adds test pins and behavior

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,10 @@
 #define PHASE_SEG_2 7
 #define BIT_TQ (1 + PHASE_SEG_1 + PHASE_SEG_2)
 #define SJW 1
+#define DEFAULT_BTL_STATE BTL_SEG1
 
 #define Fosc 16000000  // 16MHz
-#define BITRATE 10000  // 500kbps
+#define BITRATE 500000  // 500kbps
 #define TIMEQUANTAPULSES (Fosc / ( BITRATE * BIT_TQ))
 #define TIMERBASE (65536 - TIMEQUANTAPULSES)
 
@@ -38,13 +39,24 @@ FLAGS_VALUE idle = ENABLED;
 FLAGS_VALUE hard_sync = DISABLED;
 FLAGS_VALUE resync = DISABLED;
 // Errors
-// TODO
+
+
+// Tests
+const int TQ_INDICATOR_PIN = 4;
+const int HARDSYNC_PIN = 5;
+const int SOFTSYNC_PIN = 6;
+const int STATE_HIGH_PIN = 7;
+const int STATE_LOW_PIN = 8;
+int tq_indicator = 0;
+int hs_indicator = 0;
+int ss_indicator = 0;
 
 // Prototipo
 void edge_detection();
 void bit_timing();
 void sample();
 void write();
+void testWriteState(BIT_TIMING_STATES target);
 
 void init_timer(){
   TCCR1A = 0;                        //confira timer para operação normal pinos OC1A e OC1B desconectados
@@ -62,27 +74,52 @@ ISR(TIMER1_OVF_vect)                              //interrupção do TIMER1
 }
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(RX_PIN, INPUT);
   pinMode(TX_PIN, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(RX_PIN), edge_detection, FALLING);
+
+  // Tests
+  pinMode(TQ_INDICATOR_PIN, OUTPUT);
+  pinMode(HARDSYNC_PIN, OUTPUT);
+  pinMode(SOFTSYNC_PIN, OUTPUT);
+  pinMode(STATE_HIGH_PIN, OUTPUT);
+  pinMode(STATE_LOW_PIN, OUTPUT);
+  digitalWrite(TQ_INDICATOR_PIN, (tq_indicator) ? HIGH : LOW);
+  digitalWrite(HARDSYNC_PIN, (hs_indicator) ? HIGH : LOW);
+  digitalWrite(SOFTSYNC_PIN, (ss_indicator) ? HIGH : LOW);
+  testWriteState(DEFAULT_BTL_STATE);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // if (write_point) {
+  // encode();
+  write();
+  // }
+  // if (sample_point) {
+  sample();
+  // decode();
+  // }
 }
 
 void edge_detection() {
   if (idle == ENABLED) {
     hard_sync = ENABLED;
+    hs_indicator = !hs_indicator;
+    digitalWrite(HARDSYNC_PIN, (hs_indicator) ? HIGH : LOW);
   } else {
     resync = ENABLED;
+    ss_indicator = !ss_indicator;
+    digitalWrite(SOFTSYNC_PIN, (ss_indicator) ? HIGH : LOW);
   }
 }
 
 void bit_timing() {
-  static BIT_TIMING_STATES state = BTL_SYNC;
+  static BIT_TIMING_STATES state = DEFAULT_BTL_STATE;  
   static int tq_count = 0;
+
+  tq_indicator = !tq_indicator;
+  digitalWrite(TQ_INDICATOR_PIN, (tq_indicator) ? HIGH : LOW);
 
   switch (state){
     case BTL_SYNC:
@@ -90,6 +127,7 @@ void bit_timing() {
       hard_sync = DISABLED;
       resync = DISABLED;
       state = BTL_SEG1;
+      testWriteState(state);
       break;
 
     case BTL_SEG1:
@@ -105,8 +143,8 @@ void bit_timing() {
         if (tq_count >= PHASE_SEG_1) {
           tq_count = 0;
           sample_point = ENABLED;
-          // write_point = DISABLED;
           state = BTL_SEG2;
+          testWriteState(state);
         }
       }
       break;
@@ -117,6 +155,7 @@ void bit_timing() {
         tq_count = 0;
         hard_sync = DISABLED;
         state = BTL_SEG1;
+        testWriteState(state);
       } else {
         if (resync == ENABLED) {
           tq_count = min(PHASE_SEG_2 + 1, tq_count + SJW);
@@ -126,10 +165,12 @@ void bit_timing() {
           tq_count = 0;
           write_point = ENABLED;
           state = BTL_SYNC;
+          testWriteState(state);
         }
         if (tq_count > PHASE_SEG_2) {
           tq_count = 0;
           state = BTL_SEG1;
+          testWriteState(state);
         }
       }
       break;
@@ -147,5 +188,20 @@ void write() {
   if (write_point == ENABLED) {
     digitalWrite(TX_PIN, write_bit);
     write_point = DISABLED;
+  }
+}
+
+void testWriteState(BIT_TIMING_STATES target) {
+  if (target == BTL_SYNC) {
+    digitalWrite(STATE_HIGH_PIN, LOW);
+    digitalWrite(STATE_LOW_PIN, LOW);
+  }
+  else if (target == BTL_SEG1) {
+    digitalWrite(STATE_HIGH_PIN, LOW);
+    digitalWrite(STATE_LOW_PIN, HIGH);
+  }
+  else if (target == BTL_SEG2) {
+    digitalWrite(STATE_HIGH_PIN, HIGH);
+    digitalWrite(STATE_LOW_PIN, LOW);
   }
 }
